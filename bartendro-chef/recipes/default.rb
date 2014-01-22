2530dc778915217b617679e896d8bae783301bd7
root <root@raspberrypi.(none)>
2014-01-22 18:02:32 +0000

    More nginx tweaks

--- bartendro-chef/recipes/default.rb
+++ bartendro-chef/recipes/default.rb
@@ -1,21 +1,31 @@
-# must be run after reboot
-#user "pi" do
-#    action: remove
-#end
+user "pi" do
+    action: remove
+end
 
 # nginx and uwsgi
-cookbook_file "/etc/nginx/sites-enabled/default" do
+cookbook_file "/etc/uwsgi/apps-available/bartendro.ini" do
+  source "bartendro.ini"
+  group "root"
+  owner "root"
+  mode "0755"
+end
+
+cookbook_file "/etc/nginx/sites-available/default" do
   source "nginx-default"
   group "root"
   owner "root"
   mode "0755"
 end
 
-cookbook_file "/etc/uwsgi/apps-available/bartendro.ini" do
-  source "bartendro.ini"
+cookbook_file "/etc/nginx/nginx.conf" do
+  source "nginx.conf"
   group "root"
   owner "root"
   mode "0755"
+end
+
+link "/etc/nginx/sites-enabled/default" do
+  to "/etc/nginx/sites-available/default"
 end
 
 git "/home/bartendro/bartendro" do
@@ -41,5 +51,17 @@ link "/etc/uwsgi/apps-enabled/bartendro.ini" do
 end
 
 service "uwsgi" do
+  action :restart
+end
+
+service "dnsmasq" do
+  action :restart
+end
+
+service "hostapd" do
+  action :restart
+end
+
+service "nginx" do
   action :restart
 end
